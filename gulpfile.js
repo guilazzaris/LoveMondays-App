41645777909bef4e4890d96e53feffccd465de09
Guilherme Lazzaris <gui.lazzaris@gmail.com>
2016-05-20 01:21:21 -0300

    Add sass style

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
 const connect = require('gulp-connect');
+const sass = require('gulp-sass');
 const runSequence  = require('run-sequence');
+
+
+gulp.task('styles', function() {
+    return gulp.src(['public/sass/*.scss'])
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('public/css'));
+});
 
 gulp.task('connect:dev', function() {
   return connect.server({
@@ -20,9 +28,13 @@ gulp.task('connect:reload', function() {
 });
 
 gulp.task('watch', function() {
-	return gulp.watch(['public/**/*', 'gulpfile.js', 'package.json'], ['connect:reload']);
+	return gulp.watch(['public/**/*', 'public/app/**/*', 'gulpfile.js', 'package.json'], ['reload']);
 });
 
 gulp.task('dev', function(callback) {
   return runSequence('connect:dev', 'watch', callback);
+});
+
+gulp.task('reload', function(callback) {
+	return runSequence('styles', 'connect:reload', callback);
 });
